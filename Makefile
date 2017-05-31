c6355e3418598b108f1fb24e4da5c07bd016d9eb
Nico Hauser <me@tyratox.ch>
2017-05-31 13:21:54 +0200

    Updated gitignore

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 WEBPACK = webpack
 BUILD_CONFIG = ./webpack-build.config.js
-
 BUILD = ./build
 
-
-
 all:
-	$(WEBPACK) --config $(BUILD_CONFIG)
+	if [ -f config.json ] && [ -f web/oauth-callback.php ]; then
+		$(WEBPACK) --config $(BUILD_CONFIG)
+		cp web/index.html $(BUILD)/index.html
+		cp config.json $(BUILD)/config.json
+	else
+		echo "Rename config-sample.json to config.json and web/oauth-callback-sample.php to web/oauth-callback.php and adjust the values.";
+	fi
 clean:
 	rm -rf $(BUILD)/
---
+++ web/oauth-callback-sample.php
@@ -0,0 +1,37 @@
+<?php
+
+$config = json_decode(file_get_contents("config.json"));
+define("CLIENT_SECRET", "jbRA3q/ibi6A0FS4yL7sV3UAf6rtjO");
+
+if(empty($_GET["code"])){
+	header("Location: /");
+	die();
+}
+
+
+$curl = curl_init($config["API_URL"] . "/oauth2/token");
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array(
+	"clientId"      => $config["CLIENT_ID"],
+	"clientSecret"  => CLIENT_SECRET,
+	"code"          => $_GET["code"],
+	"grant_type"    => "authorization_code"
+)));
+
+$response = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+if($response && $status == 200){
+	$json = json_decode($response);
+	$token = $json->access_token;
+	
+	header("Location: /auth/callback?token=" . $token->token . "&clientId=" . $token->clientId . "&userId=" . $token->userId . "&expires=" . $token->expires);
+}else{
+	header("Location: /?status=" . $status . "&response=" . $response);
+}
+
+	
+?>
